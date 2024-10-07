cbad043e4c4c6cff6435ee7def7ab585eb2cd0e0
kaaneneskpc <kaaneneskpc1@gmail.com>
2024-10-07 08:58:16 +0300

    change apikEY

--- app/src/main/java/com/kaaneneskpc/geminiaichatbot/data/ChatData.kt
+++ app/src/main/java/com/kaaneneskpc/geminiaichatbot/data/ChatData.kt
@@ -3,13 +3,12 @@ package com.kaaneneskpc.geminiaichatbot.data
 import android.graphics.Bitmap
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.content
-import com.kaaneneskpc.geminiaichatbot.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 object ChatData {
 
-    private val api_key = BuildConfig.apiKey
+    private val api_key = "AIzaSyA7m2oUoaexoqu1t-hUuMEFz0zwRMjVlmA"
 
     suspend fun getResponse(prompt: String): Chat {
         val generativeModel = GenerativeModel(
