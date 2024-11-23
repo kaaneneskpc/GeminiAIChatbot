1aa573176ac7044614bcc0d73cc8923cb3c24ba6
kaaneneskpc <kaaneneskpc1@gmail.com>
2024-11-24 00:27:58 +0300

    -Starter created

--- app/src/main/java/com/kaaneneskpc/geminiaichatbot/data/ChatData.kt
+++ app/src/main/java/com/kaaneneskpc/geminiaichatbot/data/ChatData.kt
@@ -3,70 +3,13 @@ package com.kaaneneskpc.geminiaichatbot.data
 import android.graphics.Bitmap
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.content
-import com.kaaneneskpc.geminiaichatbot.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 object ChatData {
 
-    private val api_key = BuildConfig.API_KEY
+    private val api_key = "AIzaSyA7m2oUoaexoqu1t-hUuMEFz0zwRMjVlmA"
 
-    suspend fun getResponse(prompt: String): Chat {
-        val generativeModel = GenerativeModel(
-            modelName = "gemini-pro", apiKey = api_key
-        )
-
-        try {
-            val response = withContext(Dispatchers.IO) {
-                generativeModel.generateContent(prompt)
-            }
-
-            return Chat(
-                prompt = response.text ?: "error",
-                bitmap = null,
-                isFromUser = false
-            )
-
-        } catch (e: Exception) {
-            return Chat(
-                prompt = e.message ?: "error",
-                bitmap = null,
-                isFromUser = false
-            )
-        }
-
-    }
-
-    suspend fun getResponseWithImage(prompt: String, bitmap: Bitmap): Chat {
-        val generativeModel = GenerativeModel(
-            modelName = "gemini-1.5-flash", apiKey = api_key
-        )
-
-        try {
-
-            val inputContent = content {
-                image(bitmap)
-                text(prompt)
-            }
-
-            val response = withContext(Dispatchers.IO) {
-                generativeModel.generateContent(inputContent)
-            }
-
-            return Chat(
-                prompt = response.text ?: "error",
-                bitmap = null,
-                isFromUser = false
-            )
-
-        } catch (e: Exception) {
-            return Chat(
-                prompt = e.message ?: "error",
-                bitmap = null,
-                isFromUser = false
-            )
-        }
-
-    }
+    //Create Your Own Model Chatbot
 
 }
--- app/src/main/java/com/kaaneneskpc/geminiaichatbot/presentation/chat/ChatViewModel.kt
+++ app/src/main/java/com/kaaneneskpc/geminiaichatbot/presentation/chat/ChatViewModel.kt
@@ -18,62 +18,10 @@ class ChatViewModel @Inject constructor() : ViewModel() {
     val chatState = _chatState.asStateFlow()
 
     fun onEvent(event: ChatUiEvent) {
-        when (event) {
-            is ChatUiEvent.SendPrompt -> {
-                if (event.prompt.isNotEmpty()) {
-                    addPrompt(event.prompt, event.bitmap)
-
-                    if (event.bitmap != null) {
-                        getResponseWithImage(event.prompt, event.bitmap)
-                    } else {
-                        getResponse(event.prompt)
-                    }
-                }
-            }
-
-            is ChatUiEvent.UpdatePrompt -> {
-                _chatState.update {
-                    it.copy(prompt = event.newPrompt)
-                }
-            }
-        }
+        //Added Event
     }
 
-    private fun addPrompt(prompt: String, bitmap: Bitmap?) {
-        _chatState.update {
-            it.copy(
-                chatList = it.chatList.toMutableList().apply {
-                    add(0, Chat(prompt, bitmap, true))
-                },
-                prompt = "",
-                bitmap = null
-            )
-        }
-    }
+    //Add Prompt from User
 
-    private fun getResponse(prompt: String) {
-        viewModelScope.launch {
-            val chat = ChatData.getResponse(prompt)
-            _chatState.update {
-                it.copy(
-                    chatList = it.chatList.toMutableList().apply {
-                        add(0, chat)
-                    }
-                )
-            }
-        }
-    }
-
-    private fun getResponseWithImage(prompt: String, bitmap: Bitmap) {
-        viewModelScope.launch {
-            val chat = ChatData.getResponseWithImage(prompt, bitmap)
-            _chatState.update {
-                it.copy(
-                    chatList = it.chatList.toMutableList().apply {
-                        add(0, chat)
-                    }
-                )
-            }
-        }
-    }
+    //Get Response from Chatbot
 }
